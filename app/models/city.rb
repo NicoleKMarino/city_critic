246c16199450aed80202eab5a119555e181020b0
Nicole Marino <nicolekmarino@hotmail.com>
2016-09-20 22:45:51 -0600

    added average star functionality

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,10 @@ class City < ApplicationRecord
   validates :name, presence: true
   belongs_to :state
   has_many :comments
+
+  def average_review
+    stars = self.comments.pluck(:stars)
+    stars.reduce(:+)/stars.count
+  end
+
 end
